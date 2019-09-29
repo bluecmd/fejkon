32ee30db98cb72696d22baba3bc6e846b6adc416
Christian Svensson <blue@cmd.nu>
2019-09-29 22:11:37 +0200

    Looping network driver and test

--- driver/src/fejkon.c
+++ driver/src/fejkon.c
@@ -4,6 +4,8 @@
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/netdevice.h>
+#include <linux/if_arp.h>
 #include <linux/pci.h>
 #include <asm/uaccess.h>
 
@@ -18,6 +20,43 @@
 #define EDU_DEVICE_ID 0x11e8
 #define QEMU_VENDOR_ID 0x1234
 
+struct fejkon_net {
+  struct pci_dev *dev;
+};
+
+static const struct header_ops net_header_ops = {
+};
+
+static const struct ethtool_ops net_ethtool_ops = {
+};
+
+static int net_open(struct net_device *net)
+{
+  netdev_notice(net, "%s up\n", net->name);
+  return 0;
+}
+
+static int net_stop(struct net_device *net)
+{
+  netdev_notice(net, "%s close\n", net->name);
+  return 0;
+}
+
+static netdev_tx_t net_tx(struct sk_buff *skb, struct net_device *net)
+{
+  netdev_notice(net, "%s tx len %d, bytes: %02x %02x %02x %02x\n",
+      net->name, skb->len, skb->data[0], skb->data[1], skb->data[2], skb->data[3]);
+  // Loop the packet back for now
+  netif_rx(skb);
+  return NETDEV_TX_OK;
+}
+
+static const struct net_device_ops net_netdev_ops = {
+  .ndo_open       = net_open,
+  .ndo_stop       = net_stop,
+  .ndo_start_xmit = net_tx,
+};
+
 static struct pci_dev *pdev;
 static void __iomem *mmio;
 
@@ -28,36 +67,77 @@ static irqreturn_t irq_handler(int irq, void *dev)
   return IRQ_HANDLED;
 }
 
+static void init_netdev(struct net_device *net)
+{
+  net->header_ops = &net_header_ops;
+  net->netdev_ops = &net_netdev_ops;
+  net->ethtool_ops = &net_ethtool_ops;
+  net->addr_len = 3;
+  net->type = ARPHRD_FCFABRIC;
+  net->mtu = 2148;
+  net->min_mtu = 2148;
+  net->flags = IFF_NOARP;
+  memset(net->broadcast, 0xff, 3);
+}
+
 static int probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
   unsigned int version;
+  int ret;
+  int i;
+  struct net_device *net;
+
   pdev = dev;
-  if (pci_enable_device(dev) < 0) {
-    dev_err(&(pdev->dev), "pci_enable_device\n");
+  ret = pci_enable_device(dev);
+  if (ret < 0) {
+    dev_err(&pdev->dev, "pci_enable_device\n");
     goto error;
   }
 
-  if (pci_request_region(dev, 0 /* bar */, KBUILD_MODNAME)) {
-    dev_err(&(pdev->dev), "pci_request_region\n");
+  ret = pci_request_region(dev, 0 /* bar */, KBUILD_MODNAME);
+  if (ret < 0) {
+    dev_err(&pdev->dev, "pci_request_region\n");
     goto error;
   }
 
   mmio = pci_iomap(pdev, 0 /* bar */, MMIO_AREA_SIZE);
-  if (request_irq(dev->irq, irq_handler, IRQF_SHARED, KBUILD_MODNAME, dev) < 0) {
-    dev_err(&(dev->dev), "request_irq\n");
+  ret = request_irq(dev->irq, irq_handler, IRQF_SHARED, KBUILD_MODNAME, dev);
+  if (ret < 0) {
+    dev_err(&dev->dev, "request_irq\n");
     goto error;
   }
 
   version = ioread32(mmio + 0x0);
   if ((version & 0xff) != 0xed) {
-    pr_debug("tried to load driver for unknown card: %02x", version & 0xff);
+    dev_dbg(&dev->dev, "tried to load driver for unknown card: %02x", version & 0xff);
     goto error;
   }
 
-  pr_info("found card with version %d.%d\n", version >> 24, (version >> 16) & 0xff);
+  dev_notice(&dev->dev, "found card with version %d.%d\n", version >> 24, (version >> 16) & 0xff);
+
+  /* card initialized, register netdev for ports */
+  /* TODO: Read from card */
+  for (i = 0; i < 1; i++) {
+    net = alloc_netdev(sizeof(*dev), "fc%d", NET_NAME_UNKNOWN, init_netdev);
+    if (net == NULL) {
+      return -ENOMEM;
+    }
+
+    SET_NETDEV_DEV(net, &dev->dev);
+    ((struct fejkon_net *)netdev_priv(net))->dev = dev;
+
+    ret = register_netdev(net);
+    if (ret) {
+      dev_err(&dev->dev, "unable to register netdev\n");
+      goto error;
+    }
+
+    dev_notice(&dev->dev, "registered port %d netdev %s\n", i, net->name);
+  }
+
   return 0;
 error:
-  return 1;
+  return ret;
 }
 
 static void remove(struct pci_dev *dev)
--- driver/test/fejkon_test.go
+++ driver/test/fejkon_test.go
@@ -1,16 +1,73 @@
 package main
 
 import (
-	"github.com/u-root/u-root/pkg/kmodule"
 	"golang.org/x/sys/unix"
 	"log"
+	"net"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/mdlayher/raw"
+	"github.com/u-root/u-root/pkg/kmodule"
+	"github.com/vishvananda/netlink"
 )
 
-func TestTest(t *testing.T) {
-	t.Errorf("Hello")
+func TestSemdPacket(t *testing.T) {
+	ifi, err := net.InterfaceByName("fc0")
+	if err != nil {
+		log.Fatalf("failed to open interface: %v", err)
+	}
+	c, err := raw.ListenPacket(ifi, 0x0 /* ethertype */, nil)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	b := []byte{1, 2, 3, 4}
+	addr := &raw.Addr{HardwareAddr: []byte{0xff, 0xff, 0xff}}
+	if _, err := c.WriteTo(b, addr); err != nil {
+		log.Fatalf("failed to write frame: %v", err)
+	}
+
+	// Continue to send this packet in the background for now
+	go func() {
+		for {
+			if _, err := c.WriteTo(b, addr); err != nil {
+				log.Fatalf("failed to write frame: %v", err)
+			}
+			time.Sleep(time.Millisecond * 100)
+		}
+	}()
+}
+
+func TestReadPacket(t *testing.T) {
+	handle, err := pcap.OpenLive("fc0", 4096, true, pcap.BlockForever)
+	if err != nil {
+		t.Fatalf("OpenLive: %v", err)
+	}
+	ps := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range ps.Packets() {
+		log.Printf("Packet: %v", packet)
+	}
+}
+
+func ifup(iface string) {
+	l, err := netlink.LinkByName(iface)
+	if err != nil {
+		log.Fatalf("Unable to get interface %s: %v", iface, err)
+	}
+	h, err := netlink.NewHandle(unix.NETLINK_ROUTE)
+	if err != nil {
+		log.Fatalf("netlink.NewHandle: %v", err)
+	}
+	defer h.Delete()
+	if err := h.LinkSetUp(l); err != nil {
+		log.Fatalf("handle.LinkSetUp: %v", err)
+	}
 }
 
 func insmod() {
@@ -35,6 +92,14 @@ func TestMain(m *testing.M) {
 	}
 
 	insmod()
+	ifup("fc0")
+
+	/* TODO: Remove */
+	out, err := exec.Command("/sbin/ip", "link").CombinedOutput()
+	log.Printf("Output: %v", string(out))
+	if err != nil {
+		log.Fatalf("iproute2: %s", err)
+	}
 
 	m.Run()
 	unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF)

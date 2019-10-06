5b29b2d8759c436f572d50b6b3b5947fe63c696e
Christian Svensson <blue@cmd.nu>
2019-10-06 13:27:29 +0200

    Add SFP debug syscon command

--- syscon.tcl
+++ syscon.tcl
@@ -26,13 +26,85 @@ if {![jtag_debug_sense_clock $m]} {
 puts " E.g:"
 puts " - master_write_32 \$m 0x000e0000 5"
 puts " - jtag_debug_reset_system \$m"
+puts " - sfp 1"
 
 puts ""
-puts [format " SFP A status: %s" [master_read_8 $m 0x1000 1]]
-# TODO: Verify that there is an SFP in the slot
-master_write_32 $m 0x1040 0x2A0
-master_write_32 $m 0x1040 0x000
-master_write_32 $m 0x1040 0x100
-master_write_32 $m 0x1048 1
 
-puts [format " SFP A data: %s" [master_read_32 $m 0x1044 1]]
+proc sfp {id} {
+  global m
+  set off [expr 0x1000 * $id]
+  puts [format " SFP status: %s" [master_read_8 $m $off 1]]
+  # TODO: Verify that there is an SFP in the slot
+  master_write_32 $m [expr $off + 0x48] 0
+  # Configure ISER, SCL_LOW, SCL_HIGH, SDA_HOLD register
+  master_write_32 $m [expr $off + 0x4c] 0x0 ; # No interrupts
+  master_write_32 $m [expr $off + 0x60] 250
+  master_write_32 $m [expr $off + 0x64] 250
+  master_write_32 $m [expr $off + 0x68] 15
+  master_write_32 $m [expr $off + 0x48] 1
+  # Read three byte @ 0x0
+  master_write_32 $m [expr $off + 0x40] 0x2A0
+  master_write_32 $m [expr $off + 0x40] 0x000
+  master_write_32 $m [expr $off + 0x40] 0x2A1
+  master_write_32 $m [expr $off + 0x40] 0x000
+  master_write_32 $m [expr $off + 0x40] 0x000
+  master_write_32 $m [expr $off + 0x40] 0x100
+
+  puts [format " SFP identifier: %s" [master_read_8 $m [expr $off + 0x44] 1]]
+  puts [format " SFP ext. identifier: %s" [master_read_8 $m [expr $off + 0x44] 1]]
+  puts [format " SFP connector: %s" [master_read_8 $m [expr $off + 0x44] 1]]
+
+  # Read vendor @ 0x14
+  master_write_32 $m [expr $off + 0x40] 0x2A0
+  master_write_32 $m [expr $off + 0x40] 0x014
+  master_write_32 $m [expr $off + 0x40] 0x2A1
+
+  master_write_32 $m [expr $off + 0x40] 0x000
+  set vendor [master_read_8 $m [expr $off + 0x44] 1]
+
+  for {set i 0} {$i < 14} {incr i} {
+    master_write_32 $m [expr $off + 0x40] 0x000
+    lappend vendor [master_read_8 $m [expr $off + 0x44] 1]
+  }
+
+  master_write_32 $m [expr $off + 0x40] 0x100
+  lappend vendor [master_read_8 $m [expr $off + 0x44] 1]
+
+  puts [format " SFP vendor: '%s'" [binary format c* $vendor]]
+
+  # Read vendor PN @ 0x28
+  master_write_32 $m [expr $off + 0x40] 0x2A0
+  master_write_32 $m [expr $off + 0x40] 0x028
+  master_write_32 $m [expr $off + 0x40] 0x2A1
+
+  master_write_32 $m [expr $off + 0x40] 0x000
+  set vendor [master_read_8 $m [expr $off + 0x44] 1]
+
+  for {set i 0} {$i < 14} {incr i} {
+    master_write_32 $m [expr $off + 0x40] 0x000
+    lappend vendor [master_read_8 $m [expr $off + 0x44] 1]
+  }
+
+  master_write_32 $m [expr $off + 0x40] 0x100
+  lappend vendor [master_read_8 $m [expr $off + 0x44] 1]
+
+  puts [format " SFP vendor PN: '%s'" [binary format c* $vendor]]
+
+  # Read vendor SN @ 0x44
+  master_write_32 $m [expr $off + 0x40] 0x2A0
+  master_write_32 $m [expr $off + 0x40] 0x044
+  master_write_32 $m [expr $off + 0x40] 0x2A1
+
+  master_write_32 $m [expr $off + 0x40] 0x000
+  set vendor [master_read_8 $m [expr $off + 0x44] 1]
+
+  for {set i 0} {$i < 14} {incr i} {
+    master_write_32 $m [expr $off + 0x40] 0x000
+    lappend vendor [master_read_8 $m [expr $off + 0x44] 1]
+  }
+
+  master_write_32 $m [expr $off + 0x40] 0x100
+  lappend vendor [master_read_8 $m [expr $off + 0x44] 1]
+
+  puts [format " SFP vendor SN: '%s'" [binary format c* $vendor]]
+}

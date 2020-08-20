8d43cdb242d1599743e5697cdde3ff12d4306a12
Christian Svensson <blue@cmd.nu>
2020-08-20 22:27:17 +0200

    timing: Include more report types
    Some types were missed in the past like recovery.

--- timing.tcl
+++ timing.tcl
@@ -6,9 +6,17 @@ report_clock_fmax_summary -file "fmax.txt" -panel_name "Fmax"
 
 foreach_in_collection op [get_available_operating_conditions] {
   set_operating_conditions $op
-  report_timing -setup -npaths 10 -detail full_path -multi_corner \
-    -file "critical_paths_$op.html" -panel_name "Critical paths for $op"
-  report_timing -setup -npaths 10 -detail full_path -multi_corner \
-    -less_than_slack 0 \
-    -append -file "violated_paths.txt" -panel_name "Violated paths for $op"
+  foreach x {setup hold recovery removal} {
+    report_timing -npaths 10 -detail full_path -multi_corner \
+      -file "critical_${x}_paths_$op.html" \
+      -panel_name "Critical $x paths for $op" \
+      -$x
+    if {[report_timing -npaths 1 -multi_corner -less_than_slack 0 -$x] \
+      ne "0 0.000"} {
+      report_timing -npaths 10 -detail full_path -multi_corner \
+        -less_than_slack 0 \
+        -append -file "violated_paths.txt" -panel_name "Violated paths for $op" \
+        -$x
+    }
+  }
 }

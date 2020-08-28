2855c358d4b702cacc0314b3b979fb5306ce1b88
Christian Svensson <blue@cmd.nu>
2020-08-29 00:58:13 +0200

    Update main.go

--- driver/cmd/fejkon-sfp/main.go
+++ driver/cmd/fejkon-sfp/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// TODO: Check for /sys/class/i2c-dev/i2c-*/name == fejkon
 	p, err := sff.Read("/dev/i2c-2")
 	if err != nil {
 		log.Fatalf("sff.Read failed: %v", err)

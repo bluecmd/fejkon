4c3d8a2593c43dae09729fbd4d2c268a603b7f0c
Christian Svensson <blue@cmd.nu>
2020-09-28 17:16:36 +0200

    wip

---
+++ utils/presubmit-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o pipefail
+
+failure=0
+
+for ip in fejkon_pcie_data pcie_msi_intr fejkon_fc_debug
+do
+  if ! make -C "ip/${ip}" test; then
+    failure=1
+  fi
+done
+
+exit $failure
